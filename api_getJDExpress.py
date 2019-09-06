454c573963b2a7d8fd8fbdb291fa5cdbe53550e6
andyren <51946816@qq.com>
2019-09-06 17:33:18 +0800

    update

--- api_getJDExpress.py
+++ api_getJDExpress.py
@@ -7,7 +7,7 @@ import re
 import time
 import requests
 import hashlib
-# from sshtunnel import SSHTunnelForwarder
+from sshtunnel import SSHTunnelForwarder
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -23,38 +23,39 @@ def md5_passwd():
     res = md.hexdigest()
     return res
 
-# def sql_cookie():
-#     with SSHTunnelForwarder(
-#             ('152.136.122.35', 22),  # B机器的配置
-#             ssh_password='bhJYTpK0Y1iJUY1f',
-#             ssh_username='root',
-#             remote_bind_address=('172.21.0.43', 3306)) as server:  # A机器的配置
-#
-#         db_connect = pymysql.connect(host='127.0.0.1',  # 此处必须是是127.0.0.1
-#                                      port=server.local_bind_port,
-#                                      user='root',
-#                                      passwd='kW@gGt9Sn8hXfVdq',
-#                                      db='competeshop_data')
-#
-#         cursor = db_connect.cursor()
-#
-#         sql1 = "SELECT cookie FROM cookie_pool ORDER BY id DESC LIMIT 1;"
-#         # 执行sql语句
-#         try:
-#             cursor.execute(sql1)
-#             li = list(cursor.fetchall())
-#             cookie = li[0][0]
-#             db_connect.commit()
-#
-#             return cookie
-#         except Exception as e:
-#             print(e)
-#             db_connect.rollback()
-#         cursor.close()
-#         db_connect.close()
+
+def sql_cookie(account_name):
+    # with SSHTunnelForwarder(
+    #         ('152.136.122.35', 22),  # B机器的配置
+    #         ssh_password='bhJYTpK0Y1iJUY1f',
+    #         ssh_username='root',
+    #         remote_bind_address=('172.21.0.43', 3306)) as server:  # A机器的配置
+
+        db_connect = pymysql.connect(host='106.75.119.186',  # 此处必须是是127.0.0.1
+                                     port=4040,
+                                     user='root',
+                                     passwd='0UY$Npuj8FdUHJky2',
+                                     db='renqibox_server')
+
+        cursor = db_connect.cursor()
+
+        sql1 = "SELECT cookie FROM cookie_pool where account='%s';" % account_name
+        # 执行sql语句
+        try:
+            cursor.execute(sql1)
+            li = list(cursor.fetchall())
+            cookie = li[0][0]
+            db_connect.commit()
+
+            return cookie
+        except Exception as e:
+            print(e)
+            db_connect.rollback()
+        cursor.close()
+        db_connect.close()
 
 
-def get_express(orderId, pickDate):
+def get_cookie(cookie, orderId, pickDate):
     url = 'https://details.jd.com/lazy/getOrderTrackInfoMultiPackage.action'
 
     headers = {
@@ -67,7 +68,7 @@ def get_express(orderId, pickDate):
         'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
         'content-length': '85',
         'content-type': 'application/x-www-form-urlencoded',
-        'cookie': 'shshshfpb=o3tpMHoeJMcdhGQVWp%2B6fcw%3D%3D; shshshfp=dee755506176edc8b6bbc40b1f6d0aee; PCSYCityID=2; areaId=2; __jdb=122270672.3.15632589421771933825658|1.1563258942; shshshsID=82908193df3d1014bcfd2145d7512e85_1_1563258978521; o2Control=webp; _tp=rYu746T0HT3BtFB4YBmoGg%3D%3D; shshshfpa=2b63f9b4-14ae-69a5-bcf2-3fefadd29f2c-1563258978; ceshi3.com=203; thor=3B6F21E0DC2FC8A5E86C0D7E1A80EB15D66248FA71F234464D569C5FCC790A1901035B22F07DED890410797654DEB70FFEE0D0E77B790403778C0388A0D757D4D6DCCD063D5DDBEE9858E4CA73C0DCF107619F2C2A9F4324755982838D10D102EDC3ABBE4F0E8E6A3C5FC5712D17A3596C543ACB7E152DF697490B9EE48463E5CFA1210DE076A22A39550DE59610DF82; _pst=51946816_m; TrackID=1Q39WHZMDLq6FuXWlbrAe0BMFgI2XpX74vKPkLvnbPIhftbfuQVYrRhJOVHh0xyHvf36spLOA0VdoceliVjncr-ioPK4P8HH6poUw0yh9tT8; unick=%E6%99%A8%E6%9B%A6%E4%B8%B6%E4%B8%B6%E4%B8%B6; __jda=122270672.15632589421771933825658.1563258942.1563258942.1563258942.1; __jdu=15632589421771933825658; ipLoc-djd=2-2817-51973-0; pinId=M26Sa3z2_EDVgNDvyOAxMw; __jdc=122270672; pin=51946816_m; __jdv=122270672|direct|-|none|-|1563258942178; ',
+        'cookie': cookie,
         'origin': 'https://details.jd.com',
         'referer': 'https://details.jd.com/normal/item.action?orderid=94859380202&PassKey=F16C63FE66ACE772E8FCF7BF9CFD2A0C',
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
@@ -81,13 +82,47 @@ def get_express(orderId, pickDate):
 
     content = requests.post(url=url, headers=headers, data=data).json()
 
-    result = content['multiPackageTrackInfoList'][0]['trackGroupInfo']['shipId']
+    print(content)
 
-    return result
+    try:
+
+        if content['multiPackageTrackInfoList'][0]['carriageInfo']['carrier'] == '京东快递':
+            data = {
+                'express_id': orderId,
+                'express_company': '京东快递'
+            }
+            return data
+
+        else:
+
+            express_id = content['multiPackageTrackInfoList'][0]['trackGroupInfo']['shipId']
+
+
+            if express_id == None:
+                express_company = ''
+
+            else:
+                express_company = content['multiPackageTrackInfoList'][0]['trackGroupInfo']['thirdName']
+
+            data = {
+                'express_id': express_id,
+                'express_company': express_company
+            }
+
+            return data
+
+    except:
+
+        data = {
+            'express_id': '',
+            'express_company': ''
+        }
+
+        return data
 
 
 @app.route('/api/getJDExpress', methods=['GET', 'POST'])
-def getJDExpress():
+def get_express():
     #如果请求方式位Post
     if request.method == "POST":
         #获取post请求参数密钥
@@ -99,14 +134,18 @@ def getJDExpress():
             # orderId 为订单单号
             # pickDate 为下单当天凌晨时间
             try:
-                # cookie = sql_cookie()
+                account_name = request.form.get('account_name')
+                print(account_name)
+                cookie = sql_cookie(account_name)
+                print(cookie)
                 orderId = request.form.get('orderId')
+                print(orderId)
                 pickDate = request.form.get('pickDate')
+                print(pickDate)
+                data = get_cookie(cookie, orderId, pickDate)
 
 
-                expressID = get_express(orderId, pickDate)
-
-                return Response(json.dumps({'msg': 'ok', 'expressID': expressID}), mimetype='application/json')
+                return Response(json.dumps({'msg': 'ok', 'expressID': data['express_id'], 'expressCompany': data['express_company']}), mimetype='application/json')
 
             except:
                 return Response(json.dumps({'msg': 'Please Contact Technology'}), mimetype='application/json')
--- test.py
+++ test.py
@@ -28,6 +28,91 @@ def md5_passwd():
     res = md.hexdigest()
     return res
 
+def get_shop_userid(item_id):
+    url = 'https://acs.m.taobao.com/h5/mtop.taobao.detail.getdetail/6.0/?data={%22itemNumId%22%3A%22'+ item_id + '%22}'
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'cache-control': 'max-age=0',
+        'cookie': 'cookie2=1acb4dc20d8272959207057bf863df78; t=edef350c191acdc23bfb6acd4246b38d; _tb_token_=e306ed76e01ee; thw=cn; enc=mF3vwSxyqWenHAyovcWRC3IvlFQs1fSBBPdNRE7TXhlgdpCvibUQJ2OBlsRpaNZE48svFgDczoDAy20SBrFbMA%3D%3D; hng=CN%7Czh-CN%7CCNY%7C156; ctoken=acoDqgTLtAhLMfJIfejzrhllor; ockeqeudmj=lFJZGUc%3D; WAPFDFDTGFG=%2B4cMKKP%2B8PI%2BKg6YiKU%2BSjhUOBTNZoiWxzY%3D; _w_app_lg=23; UM_distinctid=16cf01bb920f9-01a47ccd605636-38617706-240000-16cf01bb921dec; cna=Sw3QFarKZA0CAd5H/M9WPX9u; v=0; lgc=vinter%5Cu4E36%5Cu6668%5Cu66E6; dnk=vinter%5Cu4E36%5Cu6668%5Cu66E6; tracknick=vinter%5Cu4E36%5Cu6668%5Cu66E6; _cc_=WqG3DMC9EA%3D%3D; tg=0; uc3=nk2=Fb73KaM5e9j1KDMB&id2=UoezTpA0pYae%2FA%3D%3D&lg2=UIHiLt3xD8xYTw%3D%3D&vt3=F8dByuPYmlNOu72VCe4%3D; csg=918a5fcf; skt=10a41f55637671aa; existShop=MTU2NzU3NTQ5Mw%3D%3D; uc4=id4=0%40UO%2B14YCAtKak0CTZ5TcNnavFNWk%2F&nk4=0%40Fw%2Bl89LIObN4fYTreMR1eR3qq4j7%2B%2BE%3D; mt=ci=17_1; _m_h5_tk=2adb733dcfd697ac2ce23c69520a2f52_1567763141479; _m_h5_tk_enc=da0fc2305f4ac855702736494609d824; linezing_session=xzbHH42ky9YG25W02HBo3bHs_1567754066925Ct1u_50; uc1=cookie16=URm48syIJ1yk0MX2J7mAAEhTuw%3D%3D&cookie21=URm48syIYB3rzvI4Dim4&cookie15=VFC%2FuZ9ayeYq2g%3D%3D&existShop=false&pas=0&cookie14=UoTaH0A1C82jtw%3D%3D&tag=8&lng=zh_CN; isg=BJeXvlNrhpwWuwIzyHIKbmyWJg0hdGs2_GEQ4unEs2bNGLda8az7jlU6fvij8EO2; l=cBaUeaCcqsFhPTxoBOCi5uI8as_OSIRAguPRwN0Mi_5pJ1TsADbOkryXUe96VjWdtG8B4QHSHov9-etXmPRgTBH8sxAR.',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+
+    }
+
+
+    content = requests.get(url=url, headers=headers).content.decode('utf8')
+
+    shop_user_id = re.findall('"userId":"(.*?)",', content)[0]
+
+    return shop_user_id
+
+
+
+def get_tm_company(shop_user_id):
+    url = 'https://hdc1.alicdn.com/asyn.htm?userId={}'
+
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+    }
+
+    try:
+        content1 = requests.get(url=url.format(shop_user_id), headers=headers).text.replace(r"\r\n", '').replace(" ",
+                                                                                                            '').replace(
+            " ", '').replace(r'\"', '').replace(r'/', '').replace('<span>', '')
+        # pattern = re.compile(r'"categoryName":"(.*?)"')
+
+        re_company = re.compile(r'公司名：<label><divclass=right>(.*?)<div>')
+        company = re_company.findall(content1)[0]
+
+        content2 = {
+            'company_name': company
+        }
+    except:
+        content2 = {}
+
+    return content2
+
+
+
+
+@app.route('/api/get_tmall_company', methods=['GET', 'POST'])
+def parse_shopuserid():
+    # 如果请求方式位Post
+    if request.method == "POST":
+        # 获取post请求参数密钥
+        secret_key = request.form.get('secret_key')
+        # 本地本地生成密钥
+        md5_key = 'test_api'
+        # 比对密钥
+        if secret_key == md5_key:
+
+            try:
+                item_id = request.form.get('item_id')
+
+                shop_user_id = get_shop_userid(item_id)
+
+                result = get_tm_company(shop_user_id)
+
+                return Response(json.dumps(result), mimetype='application/json')
+
+            except:
+                return Response(json.dumps({'msg': '联系管理员'}), mimetype='application/json')
+        else:
+
+            return Response(json.dumps({'msg': '密钥不正确'}), mimetype='application/json')
+
+    elif request.method == "GET":
+        return Response(json.dumps({'msg': 'Request Method Is Wrong!'}), mimetype='application/json')
+
+
+
+
+
 @app.route('/api/jd_express_num', methods=['GET', 'POST'])
 def parse_shopuserid():
     #如果请求方式位Post
