e1ea399965a7af2c8adc8dbca900844bb8ec6eee
andyren <51946816@qq.com>
2019-09-07 10:29:51 +0800

    update

--- test.py
+++ test.py
@@ -28,14 +28,17 @@ def md5_passwd():
     res = md.hexdigest()
     return res
 
-def get_shop_userid(item_id):
-    url = 'https://acs.m.taobao.com/h5/mtop.taobao.detail.getdetail/6.0/?data={%22itemNumId%22%3A%22'+ item_id + '%22}'
+
+def shopid_get_userid(shopid):
+    url = 'https://favorite.taobao.com/popup/add_collection.htm?id={}&itemid={}&itemtype=0&ownerid=7c4deab98fd1e2bf3db7e205ff3cf587&scjjc=2'.format(
+        shopid, shopid)
+
     headers = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
         'accept-encoding': 'gzip, deflate, br',
         'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
         'cache-control': 'max-age=0',
-        'cookie': 'cookie2=1acb4dc20d8272959207057bf863df78; t=edef350c191acdc23bfb6acd4246b38d; _tb_token_=e306ed76e01ee; thw=cn; enc=mF3vwSxyqWenHAyovcWRC3IvlFQs1fSBBPdNRE7TXhlgdpCvibUQJ2OBlsRpaNZE48svFgDczoDAy20SBrFbMA%3D%3D; hng=CN%7Czh-CN%7CCNY%7C156; ctoken=acoDqgTLtAhLMfJIfejzrhllor; ockeqeudmj=lFJZGUc%3D; WAPFDFDTGFG=%2B4cMKKP%2B8PI%2BKg6YiKU%2BSjhUOBTNZoiWxzY%3D; _w_app_lg=23; UM_distinctid=16cf01bb920f9-01a47ccd605636-38617706-240000-16cf01bb921dec; cna=Sw3QFarKZA0CAd5H/M9WPX9u; v=0; lgc=vinter%5Cu4E36%5Cu6668%5Cu66E6; dnk=vinter%5Cu4E36%5Cu6668%5Cu66E6; tracknick=vinter%5Cu4E36%5Cu6668%5Cu66E6; _cc_=WqG3DMC9EA%3D%3D; tg=0; uc3=nk2=Fb73KaM5e9j1KDMB&id2=UoezTpA0pYae%2FA%3D%3D&lg2=UIHiLt3xD8xYTw%3D%3D&vt3=F8dByuPYmlNOu72VCe4%3D; csg=918a5fcf; skt=10a41f55637671aa; existShop=MTU2NzU3NTQ5Mw%3D%3D; uc4=id4=0%40UO%2B14YCAtKak0CTZ5TcNnavFNWk%2F&nk4=0%40Fw%2Bl89LIObN4fYTreMR1eR3qq4j7%2B%2BE%3D; mt=ci=17_1; _m_h5_tk=2adb733dcfd697ac2ce23c69520a2f52_1567763141479; _m_h5_tk_enc=da0fc2305f4ac855702736494609d824; linezing_session=xzbHH42ky9YG25W02HBo3bHs_1567754066925Ct1u_50; uc1=cookie16=URm48syIJ1yk0MX2J7mAAEhTuw%3D%3D&cookie21=URm48syIYB3rzvI4Dim4&cookie15=VFC%2FuZ9ayeYq2g%3D%3D&existShop=false&pas=0&cookie14=UoTaH0A1C82jtw%3D%3D&tag=8&lng=zh_CN; isg=BJeXvlNrhpwWuwIzyHIKbmyWJg0hdGs2_GEQ4unEs2bNGLda8az7jlU6fvij8EO2; l=cBaUeaCcqsFhPTxoBOCi5uI8as_OSIRAguPRwN0Mi_5pJ1TsADbOkryXUe96VjWdtG8B4QHSHov9-etXmPRgTBH8sxAR.',
+        'cookie': 'cookie2=1acb4dc20d8272959207057bf863df78; t=edef350c191acdc23bfb6acd4246b38d; _tb_token_=e306ed76e01ee; thw=cn; enc=mF3vwSxyqWenHAyovcWRC3IvlFQs1fSBBPdNRE7TXhlgdpCvibUQJ2OBlsRpaNZE48svFgDczoDAy20SBrFbMA%3D%3D; hng=CN%7Czh-CN%7CCNY%7C156; ctoken=acoDqgTLtAhLMfJIfejzrhllor; UM_distinctid=16cf01bb920f9-01a47ccd605636-38617706-240000-16cf01bb921dec; cna=Sw3QFarKZA0CAd5H/M9WPX9u; v=0; lgc=vinter%5Cu4E36%5Cu6668%5Cu66E6; dnk=vinter%5Cu4E36%5Cu6668%5Cu66E6; tracknick=vinter%5Cu4E36%5Cu6668%5Cu66E6; tg=0; mt=ci=17_1; linezing_session=xzbHH42ky9YG25W02HBo3bHs_1567754066925Ct1u_50; _m_h5_tk=10a0228f0ea8cde792f8e5457dcf9bdb_1567831579777; _m_h5_tk_enc=17fcf04af613ad03705abb901fcf00e4; unb=1671216250; uc3=nk2=Fb73KaM5e9j1KDMB&id2=UoezTpA0pYae%2FA%3D%3D&lg2=V32FPkk%2Fw0dUvg%3D%3D&vt3=F8dByuPd0a7NznW4X3c%3D; csg=5f471205; cookie17=UoezTpA0pYae%2FA%3D%3D; skt=cba6bfe6fc0bf058; existShop=MTU2NzgyMTkxNw%3D%3D; uc4=nk4=0%40Fw%2Bl89LIObN4fYTreMR1eR3nMLYjXpw%3D&id4=0%40UO%2B14YCAtKak0CTZ5TcNkMhhiSpA; _cc_=VT5L2FSpdA%3D%3D; _l_g_=Ug%3D%3D; sg=%E6%9B%A600; _nk_=vinter%5Cu4E36%5Cu6668%5Cu66E6; cookie1=ACrmID%2FLnLGRkDJCgA%2BtXaogt9EDXM0O%2FtHQiu9LXS4%3D; uc1=cookie16=URm48syIJ1yk0MX2J7mAAEhTuw%3D%3D&cookie21=U%2BGCWk%2F7p4mBoUyS4E9C&cookie15=UtASsssmOIJ0bQ%3D%3D&existShop=false&pas=0&cookie14=UoTaH0%2BVgXlJmA%3D%3D&tag=8&lng=zh_CN; isg=BOvrvjc3MkF6U263XB4eoqDyegkVqP-KwMU8Zl1oxyqB_Ate5dCP0oleUnw3R1d6; l=cBaUeaCcqsFhP3ntBOCanurza77OSIRYYuPzaNbMi_5dK6T6GWQOkrqAQF96VjWdt28B4QHSHov9-etXZr7rczyvXCDc.',
         'sec-fetch-mode': 'navigate',
         'sec-fetch-site': 'none',
         'sec-fetch-user': '?1',
@@ -44,12 +47,11 @@ def get_shop_userid(item_id):
 
     }
 
-
     content = requests.get(url=url, headers=headers).content.decode('utf8')
 
-    shop_user_id = re.findall('"userId":"(.*?)",', content)[0]
+    userid = re.findall('sellerId: (.*?),', content)[0]
 
-    return shop_user_id
+    return userid
 
 
 def get_tm_company(shop_user_id):
@@ -76,7 +78,8 @@ def get_tm_company(shop_user_id):
 
     return content2
 
-# a = get_shop_userid('599535180408')
+
+# a = shopid_get_userid('578377242850')
 # b = get_tm_company(a)
 # print(b)
 # exit()
@@ -96,10 +99,10 @@ def parse_shopuserid():
         if secret_key == md5_key:
 
             try:
-                item_id = request.form.get('item_id')
-                print('item_id：%s' % item_id)
+                shop_id = request.form.get('item_id')
+                print('item_id：%s' % shop_id)
 
-                shop_user_id = get_shop_userid(item_id)
+                shop_user_id = shopid_get_userid(shop_id)
                 print('shop_user_id：%s' % shop_user_id)
 
                 result = get_tm_company(shop_user_id)
